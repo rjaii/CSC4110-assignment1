8159d8042b95e3c2227f62c49c7214c5a55cd822
Robert Arnold <gi6195@wayne.edu>
2019-09-16 21:09:55 -0400

    backtracking

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,16 +1,17 @@
-// Simple C++ program to display "Hello World"
-
-// Header file for input output functions
-#include<iostream>
-
-using namespace std;
-
-// main function -
-// where the execution of program begins
-int main()
-{
-    // prints hello world
-    cout<<"Testing git merge!!"; 
-
-    return 0;
-}
+// Simple C++ program to display "Hello World" 
+// Author: Robert Arnold <gi6195@wayne.edu>
+// Header file for input output functions 
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Testing git merge!"; 
+      
+    return 0; 
+} 
+>>>>>>> master
